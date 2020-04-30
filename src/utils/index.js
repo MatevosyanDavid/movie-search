2b8fffcf2ff7ab8be65816cb230d7e06a2aaa317
David Matevosyan <dav.matevosyan@gmail.com>
2020-04-30 20:06:27 +0400

    Add wthSuspense in body

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,14 +1,16 @@
 import noop from './noop';
 import getTitle from './getTitle';
 import getAllData from './getAllData.js';
+import withSuspense from './withSuspense';
 import transformData from './transformData';
 import { loadState, saveState } from './persist';
 
 export {
   noop,
   getTitle,
+  loadState,
   saveState,
   getAllData,
-  loadState,
+  withSuspense,
   transformData,
 }
