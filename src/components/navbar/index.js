7f8837996d19afdfc00f527472aeaf1c1821afcd
David Matevosyan <dav.matevosyan@gmail.com>
2021-05-31 20:19:08 +0400

    Add v2

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import classNames from 'classnames';
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 
-import Link from 'components/link'
+import Link from 'components/link';
 import SearchForm from 'components/searchForm';
 
 function Navbar() {
@@ -13,7 +13,7 @@ function Navbar() {
     <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
       <div className="container">
         <NavLink to="/movies" className="navbar-brand">
-          Search Movies
+          Search Movies V2
         </NavLink>
         <button className="navbar-toggler" onClick={showMenuHandler}>
           <span className="navbar-toggler-icon"></span>
@@ -21,16 +21,8 @@ function Navbar() {
 
         <div className={classNames('navbar-collapse', { collapse: !isVisible })}>
           <ul className="navbar-nav mr-auto">
-            <Link
-              to="/movies"
-              label="Movies"
-              showMenuHandler={showMenuHandler}
-            />
-            <Link
-              to="/favorites"
-              label="Favorites"
-              showMenuHandler={showMenuHandler}
-            />
+            <Link to="/movies" label="Movies" showMenuHandler={showMenuHandler} />
+            <Link to="/favorites" label="Favorites" showMenuHandler={showMenuHandler} />
           </ul>
           <SearchForm setIsVisivle={setIsVisivle} />
         </div>
