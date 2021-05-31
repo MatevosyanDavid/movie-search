b5c63594cdd14fb32a1ca3fdb31e9d9fe0e3e227
David Matevosyan <dav.matevosyan@gmail.com>
2021-05-31 17:37:43 +0400

    Add PWA

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 
 import App from 'app';
-import * as serviceWorker from 'serviceWorker';
+import * as serviceWorker from 'serviceWorkerRegistration';
 
 import 'index.css';
 
@@ -10,10 +10,10 @@ ReactDOM.render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
